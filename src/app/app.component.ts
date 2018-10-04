d91c65af2b83221e0bcd3056d4f0b4d8b9238b0d
java-sandeep <er.pal.sandeep@outlook.com>
2018-10-04 12:21:53 +0530

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { UsersService } from './users.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit, OnDestroy {
+  
+  // variables
+  user1Activated = false;
+  user2Activated = false;
+
+  // unscribevarable
+  userSubjectUnscribe : Subscription;
+
+  constructor(private _usersService: UsersService) {}
+
+  ngOnInit() {
+   this.userSubjectUnscribe =  this._usersService.userActivated.subscribe(
+      (id: number) => {
+        if (id === 1) {
+          this.user1Activated = true;
+        } else if (id === 2) {
+          this.user2Activated = true;
+        }
+      }
+    );
+  }
+
+  ngOnDestroy(){
+    this.userSubjectUnscribe.unsubscribe();
+  }
 }
